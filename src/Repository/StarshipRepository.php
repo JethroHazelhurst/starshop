d8c59f31aa92e81ba7801a5bd7b4935e0d8a78de
Jethro Hazelhurst <jhazelhurst2@gmail.com>
2026-05-26 21:33:58 +0100

    Adding a fetch method to the starship api

---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Starship;
+use Psr\Log\LoggerInterface;
+
+class StarshipRepository
+{
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    public function findAll(): array
+    {
+        $this->logger->info('Starship collection retrieved');
+        return [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q'
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
+            ),
+            new Starship(
+                3,
+                'Test Ship 9',
+                'Test Ship 9',
+                'Test Captain 9',
+                'Test description 9',
+            ),
+        ];
+    }
+
+    public function find(int $id): ?Starship
+    {
+        foreach ($this->findAll() as $starship) {
+            if ($starship->getId() === $id) {
+                return $starship;
+            }
+        }
+
+        return null;
+    }
+}
